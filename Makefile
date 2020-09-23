47c2d706e290d3cb4cf68ebc91346dcdacafc50f
grumbach <anselmega@gmail.com>
2020-09-23 15:30:57 +0200

    [ADD] rust tests

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ python:
 	cargo build --features "python" --release
 	cp -f ./target/release/libkafka.dylib ./kafka.so
 	python tests/python_test.py
+
+rust:
+	cargo test
--- tests/lib_kafka_test.rs
+++ tests/lib_kafka_test.rs
@@ -1,6 +1,19 @@
-use libkafka::{produce, consume};
+use kafka::{produce, consume};
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
 
-fn main() {
-    produce("KAFKA_TOPIC", "hello world");
-    consume("KAFKA_TOPIC");
+fn get_random_string(len: usize) -> String {
+    thread_rng().sample_iter(&Alphanumeric).take(len).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_produce_consume() {
+        let message = get_random_string(12);
+        assert_eq!(Ok(()), produce("KAFKA_TOPIC", &message));
+        assert_eq!(Ok(message), consume("KAFKA_TOPIC"));
+    }
 }
