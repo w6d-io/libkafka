ae00986c203b73d5e7edd442aca2506a0d58aed1
Nicolas Haquet <nicolas.haquet@w6d.io>
2022-02-18 15:45:50 +0100

    [ADD] compilation features

--- Makefile
+++
@@ -1,9 +0,0 @@
-all: rust python
-
-python:
-	cargo build --features "python" --release
-	cp -f ./target/release/libkafka.dylib ./kafka.so
-	python tests/python_test.py
-
-rust:
-	cargo test
--- src/consumer.rs
+++ src/consumer.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashMap, time::Duration};
 
+#[cfg(any(feature = "kafka_debug", test))]
+use rdkafka::message::{OwnedMessage, Timestamp};
 use rdkafka::{
     config::{ClientConfig, FromClientConfig},
     consumer::Consumer,
@@ -7,9 +9,12 @@ use rdkafka::{
 };
 
 use crate::error::{KafkaError, Result};
+
 pub use rdkafka::consumer::BaseConsumer;
-#[cfg(feature = "async")]
-pub use rdkafka::consumer::{StreamConsumer, MessageStream};
+#[cfg(any(feature = "async", test))]
+pub use rdkafka::consumer::StreamConsumer;
+#[cfg(any(feature = "async", test))]
+use rdkafka::consumer::MessageStream;
 
 #[derive(Debug)]
 pub struct KafkaConsumer<T: Consumer> {
@@ -37,7 +42,6 @@ impl<T: Consumer + FromClientConfig> KafkaConsumer<T> {
             client_config.set(opt, val);
         }
         let consumer: T = client_config.create()?;
-        #[cfg(any(not(feature = "kafka_debug"), not(test)))]
         consumer.subscribe(topic_name)?;
         Ok(KafkaConsumer {
             consumer_type: consumer,
@@ -46,13 +50,12 @@ impl<T: Consumer + FromClientConfig> KafkaConsumer<T> {
 }
 
 impl KafkaConsumer<BaseConsumer> {
-
     ///Extract a message frome a BaseConsume.
     ///If the timeout is none this function block until a message is received.
     pub fn consume(&self, timeout: Option<Duration>) -> Result<Option<String>> {
         let payload = match self.consumer_type.poll(timeout) {
             Some(Ok(p)) => p,
-            Some(Err(e)) =>  return Err(KafkaError::RDKafkaError(e)),
+            Some(Err(e)) => return Err(KafkaError::RDKafkaError(e)),
             None => return Ok(None),
         };
 
@@ -64,13 +67,26 @@ impl KafkaConsumer<BaseConsumer> {
         Ok(Some(msg))
     }
 }
-#[cfg(feature = "async")]
-impl KafkaConsumer<StreamConsumer> {
 
+#[cfg(any(feature = "async", test))]
+impl KafkaConsumer<StreamConsumer> {
     ///Extract a message frome a StreamConsumer.
     ///This function block until a message is received.
+    ///If debug_kafka feature is enabled only return a debug message,
+    ///only use this for testing purpose.
     pub async fn consume(&self) -> Result<String> {
+        #[cfg(not(any(feature = "kafka_debug", test)))]
         let payload = self.consumer_type.recv().await?;
+        #[cfg(any(feature = "kafka_debug", test))]
+        let payload = OwnedMessage::new(
+            Some("debug".as_bytes().to_vec()),
+            None,
+            "debug".to_owned(),
+            Timestamp::NotAvailable,
+            1,
+            1,
+            None,
+        );
         let msg = match payload.payload_view::<str>() {
             None => return Err(KafkaError::EmptyMsgError),
             Some(Ok(s)) => s.to_owned(),
@@ -79,12 +95,13 @@ impl KafkaConsumer<StreamConsumer> {
         Ok(msg)
     }
 
-    ///Constructs a stream that yields messages from this consumer. 
-    pub fn stream(&self) -> MessageStream<'_>{
+    ///Constructs a stream that yields messages from this consumer.
+    ///To use this stream it is recomended to use a library that implements stream utilities
+    ///like futures or tokio_stream.
+    pub fn stream(&self) -> MessageStream<'_> {
         self.consumer_type.stream()
-    } 
+    }
 }
-
 
 #[cfg(test)]
 mod consumer_test {
@@ -96,15 +113,31 @@ mod consumer_test {
         KafkaConsumer::<BaseConsumer>::new(default_config("test", "test"), &["test"]).unwrap();
     }
 
+    #[test]
+    fn test_base_consumer_consume() {
+        let consumer =
+            KafkaConsumer::<BaseConsumer>::new(default_config("test", "test"), &["test"]).unwrap();
+        let msg = consumer.consume(Some(Duration::from_millis(0))).unwrap();
+        assert_eq!(msg, None);
+    }
+
     #[tokio::test]
     async fn test_stream_consumer_new() {
         KafkaConsumer::<StreamConsumer>::new(default_config("test", "test"), &["test"]).unwrap();
     }
 
-    #[test]
-    fn test_base_consumer_consume() {
-        let consumer = KafkaConsumer::<BaseConsumer>::new(default_config("test", "test"), &["test"]).unwrap();
-        let msg = consumer.consume(Some(Duration::from_millis(0))).unwrap();
-        assert_eq!(msg, None);
+    #[tokio::test]
+    async fn test_stream_consumer_consume() {
+        let consumer =
+            KafkaConsumer::<StreamConsumer>::new(default_config("test", "test"), &["test"])
+                .unwrap();
+        let msg = consumer.consume().await.unwrap();
+        assert_eq!(msg, "debug")
+    }
+    #[tokio::test]
+    async fn test_stream_consumer_stream(){
+        let consumer = KafkaConsumer::<StreamConsumer>::new(default_config("test", "test"), &["test"])
+            .unwrap();
+        consumer.stream();
     }
 }
